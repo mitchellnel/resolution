e11f46c5f704b8c0bd6b9d394c1a819299969c7f
mitchellnel <mitchellmclin@gmail.com>
2023-02-02 23:53:43 -0500

    Initialise basic Express and TypeScript server

---
+++ server/dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ path: "./.env.development.local" });
+const app = (0, express_1.default)();
+const port = process.env.PORT_NUM;
+app.get("/", (_, res) => {
+    res.send("Express + TypeScript Server");
+});
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
