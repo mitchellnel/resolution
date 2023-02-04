4998aca358148d9177118ae087bb5a079cba1c0e
mitchellnel <mitchellmclin@gmail.com>
2023-02-03 19:44:32 -0800

    Add Firebase to backend

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,31 @@ dotenv.config({ path: "./.env.development.local" });
 const app: Express = express();
 const port = process.env.PORT_NUM;
 
+/* FIREBASE SETUP */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAseiGrImJilP9immR_yspzVF9LUOGZhI",
+  authDomain: "resolution-bcmmz.firebaseapp.com",
+  projectId: "resolution-bcmmz",
+  storageBucket: "resolution-bcmmz.appspot.com",
+  messagingSenderId: "132359590757",
+  appId: "1:132359590757:web:6c2bf789203dd4873d478c",
+  measurementId: "G-GP51SC0WBL",
+};
+
+// Initialize Firebase
+const firebase_app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase_app);
+
+/* */
+
 app.get("/", (_: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
