e67a81d9cd4020718bb7e1151355a27e1058cd0c
mitchellnel <56947176+mitchellnel@users.noreply.github.com>
2023-02-14 18:50:22 -0800

    Revert "Create goal database (#14)"
    This reverts commit af66cdfd2ce7e06ac56c34c9e50cbf182f07b3e7.

--- server/index.ts
+++ server/index.ts
@@ -6,21 +6,9 @@ dotenv.config({ path: "./.env.development.local" });
 const app: Express = express();
 const PORT_NUM = 3333;
 
-app.use(express.json());
-
 /* FIREBASE SETUP */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {
-  getDatabase,
-  ref,
-  set,
-  child,
-  get,
-  push,
-  update,
-  remove,
-} from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -34,14 +22,11 @@ const firebaseConfig = {
   messagingSenderId: "132359590757",
   appId: "1:132359590757:web:6c2bf789203dd4873d478c",
   measurementId: "G-GP51SC0WBL",
-  databaseURL: "https://resolution-bcmmz-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(firebaseApp);
+// @ts-ignore
+const firebase_app = initializeApp(firebaseConfig);
 
 /* */
 
@@ -50,118 +35,15 @@ app.get("/", (_: Request, res: Response) => {
 });
 
 app.get("/api/time", (_: Request, res: Response) => {
-  const dateObj = new Date();
-  const currTime =
-    String(dateObj.getHours()).padStart(2, "0") +
+  const date_obj = new Date();
+  const curr_time =
+    String(date_obj.getHours()).padStart(2, "0") +
     ":" +
-    String(dateObj.getMinutes()).padStart(2, "0") +
+    String(date_obj.getMinutes()).padStart(2, "0") +
     ":" +
-    String(dateObj.getSeconds()).padStart(2, "0");
+    String(date_obj.getSeconds()).padStart(2, "0");
 
-  res.json({ message: currTime });
-});
-
-// DB CRUD Test Endpoints
-app.post("/api/create", async (req: Request, res: Response) => {
-  const data = req.body;
-  const jsonData = JSON.stringify(data);
-
-  // add data to the DB
-  try {
-    await set(ref(database, "more/sample_data/"), jsonData);
-    res.send(`Data Received: ${jsonData}\n\t... Data added to DB!`);
-  } catch (err) {
-    console.log(err);
-    res.send(
-      `Data Received: ${jsonData}\n\t... Data could not be added to the DB ...`
-    );
-  }
-});
-
-app.get("/api/read", async (req: Request, res: Response) => {
-  // NOTE: should use onValue and return some listener -- maybe do this client side?
-
-  // find out what DB path to read from
-  // reject request if path not included
-  if (req.query.path === undefined) {
-    res.send("No path parameter sent");
-    return;
-  }
-
-  const path = req.query.path as string;
-
-  console.log(path);
-
-  // get a snapshot of the data currently at the ref and path
-  try {
-    const snapshot = await get(child(ref(database), path));
-
-    // data may not be available at ref and path
-    if (snapshot.exists()) {
-      res.send(snapshot.val());
-    } else {
-      res.send("No data available");
-    }
-  } catch (err) {
-    console.log(err);
-    res.send("Read was unsuccessful ...");
-  }
-});
-
-app.post("/api/update", async (req: Request, res: Response) => {
-  // push used instead of update -- makes more sense to "push" a new child onto the parent JSON node
-
-  // get the path to push to
-  // reject request if path not included
-  const data = req.body;
-  const pushPath: string = data["pushPath"] ?? "";
-
-  if (pushPath === "") {
-    res.send("No push path given");
-    return;
-  }
-
-  // create the data to push
-  const pushedData = {
-    number: Math.random(),
-  };
-
-  // get a key for the new child
-  const childKey = push(child(ref(database), pushPath)).key;
-
-  // NOTE: you can simultaneously push multiple updates just by adding another field to the updates
-  //  object
-  const updates: any = {};
-  updates[pushPath + childKey] = pushedData;
-
-  try {
-    await update(ref(database), updates);
-    res.send("Update successful!");
-  } catch (err) {
-    console.log(err);
-    res.send("Update not successful...");
-  }
-});
-
-app.post("/api/delete", async (req: Request, res: Response) => {
-  // get the path to delete
-  // reject request if path not included
-  const data = req.body;
-  const deletePath: string = data["deletePath"] ?? "";
-
-  if (deletePath === "") {
-    res.send("No delete path given");
-    return;
-  }
-
-  // delete data at the path
-  try {
-    await remove(ref(database, deletePath));
-    res.send("Deletion successful!");
-  } catch (err) {
-    console.log(err);
-    res.send("Deletion was not successful ...");
-  }
+  res.json({ message: "The current time is " + curr_time });
 });
 
 app.listen(PORT_NUM, () => {
