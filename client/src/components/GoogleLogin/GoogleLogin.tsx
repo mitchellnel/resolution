6c1bb3d365a594431e44681cdafcca0d4afbdf27
mitchellnel <mitchellmclin@gmail.com>
2023-02-15 17:01:12 -0800

    Stylise sign in button with mouseover effects

--- client/src/components/GoogleLogin/GoogleLogin.tsx
+++ client/src/components/GoogleLogin/GoogleLogin.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "@mui/material/Button";
 
 import { ReactComponent as GoogleLogo } from "../../assets/google_logo.svg";
+import { ReactComponent as GoogleLogoWhite } from "../../assets/google_logo_white.svg";
 
 function GoogleLogin() {
+  const [buttonMouseover, setButtonMouseover] = useState<Boolean>(false);
+
   return (
     <>
       <Button
         variant="contained"
         size="large"
-        startIcon={<GoogleLogo />}
+        startIcon={buttonMouseover ? <GoogleLogoWhite /> : <GoogleLogo />}
         sx={{
           backgroundColor: "#f8f8f8",
           color: "#5485eb",
@@ -18,7 +21,13 @@ function GoogleLogin() {
           fontSize: "1.2rem",
           paddingBottom: "15px",
           paddingTop: "15px",
+          "&:hover": {
+            backgroundColor: "#5485eb",
+            color: "#f8f8f8",
+          },
         }}
+        onMouseOver={() => setButtonMouseover(true)}
+        onMouseOut={() => setButtonMouseover(false)}
       >
         Sign in with Google
       </Button>
