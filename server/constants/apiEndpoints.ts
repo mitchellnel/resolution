1e25436d757d760de7e50a025c87a6f8346416e4
mitchellnel <mitchellmclin@gmail.com>
2023-03-10 20:53:58 -0800

    Fix merge-related issue

--- server/constants/apiEndpoints.ts
+++ server/constants/apiEndpoints.ts
@@ -10,12 +10,6 @@ const API_COMPLETE_GOAL_ENDPOINT = "/api/complete-goal";
 const API_UPDATE_GOAL_DESCRIPTION_ENDPOINT = "/api/update-goal-description";
 const API_DELETE_GOAL_ENDPOINT = "/api/delete-goal";
 
-const API_CREATE_GOAL_ENDPOINT = "/api/create-goal";
-const API_READ_GOAL_ENDPOINT = "/api/read-goal";
-const API_COMPLETE_GOAL_ENDPOINT = "/api/complete-goal";
-const API_UPDATE_GOAL_DESCRIPTION_ENDPOINT = "/api/update-goal-description";
-const API_DELETE_GOAL_ENDPOINT = "/api/delete-goal";
-
 export {
   API_CREATE_RESOLUTION_ENDPOINT,
   API_READ_RESOLUTION_ENDPOINT,
