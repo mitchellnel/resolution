dae9a4126a5dae6e1c85d3c158ff001118f890f1
mitchellnel <mitchellmclin@gmail.com>
2023-03-12 04:52:32 -0700

    Add calendar sign-in

--- client/src/components/GoogleLogin/GoogleSignInButton.tsx
+++ client/src/components/GoogleLogin/GoogleSignInButton.tsx
@@ -3,22 +3,23 @@ import { useState } from "react";
 import Button from "@mui/material/Button";
 
 // Firebase
-import {
-  GoogleAuthProvider,
-  signInWithRedirect,
-} from "firebase/auth";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 import { auth } from "../../utils/firebase";
 
 import { ReactComponent as GoogleLogo } from "../../assets/google_logo.svg";
 import { ReactComponent as GoogleLogoWhite } from "../../assets/google_logo_white.svg";
 
 import GoogleButtonStyling from "./GoogleButtonStyling";
+import apiCalendar from "../../calendar/googleCalendar";
 
 async function redirectToSignIn() {
   const provider: GoogleAuthProvider = new GoogleAuthProvider();
 
   // add access to Google Calendar API -- view and edit events
   provider.addScope("https://www.googleapis.com/auth/calendar.events");
+
+  // calendar API sign-in
+  apiCalendar.handleAuthClick();
 
   // redirect user to sign in page
   await signInWithRedirect(auth, provider);
