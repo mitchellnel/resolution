6d7704b9d78327d03bfcd45a7034ea3f3869bad1
mitchellnel <mitchellmclin@gmail.com>
2023-03-12 04:55:00 -0700

    Add calendar sign-out invocation

--- client/src/components/GoogleLogin/GoogleSignOutButton.tsx
+++ client/src/components/GoogleLogin/GoogleSignOutButton.tsx
@@ -2,12 +2,15 @@ import Button from "@mui/material/Button";
 
 // Firebase
 import { signOut } from "firebase/auth";
+import apiCalendar from "../../calendar/googleCalendar";
 import { auth } from "../../utils/firebase";
 
 import GoogleButtonStyling from "./GoogleButtonStyling";
 
 function GoogleSignOutButton() {
   async function signOutUser() {
+    apiCalendar.handleSignoutClick();
+
     await signOut(auth);
   }
 
