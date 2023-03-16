5818712a166656c39561c5de683995e895c8d492
mitchellnel <mitchellmclin@gmail.com>
2023-03-15 19:22:39 -0700

    Set up CORS to allow requests from frontend deployment

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,14 @@
+import cors, { CorsOptions } from "cors";
+
 import app from "./app";
 
 const PORT_NUM = 3333;
+
+const corsOptions: CorsOptions = {
+  origin: "https://mitchellnel.github.io",
+};
+
+app.use(cors(corsOptions));
 
 app.listen(PORT_NUM, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT_NUM}`);
